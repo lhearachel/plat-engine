8d41fdcc9180822c7bda7d42a63e1742c6ef6533
Lhea (Rachel) <lhearachel@proton.me>
2023-05-24 15:22:52 -0700

    Refactor battle server header + separate heap header

--- include/global.h
+++ include/global.h
@@ -2,6 +2,7 @@
 #define __GLOBAL_H
 
 #include "config.h"
+#include "heap.h"
 #include "typedefs.h"
 
 #define NELEMS(array)       (sizeof(array) / sizeof(array[0]))
@@ -19,26 +20,5 @@
  * sequence iteration.
  */
 u16   __attribute__((long_call)) GF_RAND(void);
-
-/**
- * @brief Free a block of memory. The heap to free the block from is determined
- * programmatically.
- * 
- * Original Function: [`free @ 0x020181C4` (ARM9)](https://github.com/JimB16/PokePlat/blob/6d4ad87550eeb40079ede6dcf5dddec5873976e4/source/arm9_RAM_21bfaf0.s#L548)
- * 
- * @param o The memory block to be freed.
- */
-void  __attribute__((long_call)) Free(void *o);
-
-/**
- * @brief Allocate a block of memory on the heap with a given size.
- * 
- * Original Function: [`malloc @ 0x02018144` (ARM9)](https://github.com/JimB16/PokePlat/blob/6d4ad87550eeb40079ede6dcf5dddec5873976e4/source/arm9_RAM_21bfaf0.s#L460)
- * 
- * @param heapID    ID of the heap which will own this allocated memory.
- * @param size      Desired size of the block to be allocated.
- * @return          Address to the allocated memory block.
- */
-void* __attribute__((long_call)) Malloc(u32 heapId, u32 size);
 
 #endif // __GLOBAL_H
---
+++ include/heap.h
@@ -0,0 +1,27 @@
+#ifndef __HEAP_H
+#define __HEAP_H
+
+#define HEAP_ID_BATTLE  5
+
+/**
+ * @brief Free a block of memory. The heap to free the block from is determined
+ * programmatically.
+ * 
+ * Original Function: [`free @ 0x020181C4` (ARM9)](https://github.com/JimB16/PokePlat/blob/6d4ad87550eeb40079ede6dcf5dddec5873976e4/source/arm9_RAM_21bfaf0.s#L548)
+ * 
+ * @param o The memory block to be freed.
+ */
+void  __attribute__((long_call)) Free(void *o);
+
+/**
+ * @brief Allocate a block of memory on the heap with a given size.
+ * 
+ * Original Function: [`malloc @ 0x02018144` (ARM9)](https://github.com/JimB16/PokePlat/blob/6d4ad87550eeb40079ede6dcf5dddec5873976e4/source/arm9_RAM_21bfaf0.s#L460)
+ * 
+ * @param heapID    ID of the heap which will own this allocated memory.
+ * @param size      Desired size of the block to be allocated.
+ * @return          Address to the allocated memory block.
+ */
+void* __attribute__((long_call)) Malloc(u32 heapId, u32 size);
+
+#endif // __HEAP_H
