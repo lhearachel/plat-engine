4bc00c28a7cbb77906bb13588981017d4ba954cb
Lhea (Rachel) <lhearachel@proton.me>
2023-06-15 14:45:15 -0700

    Cleaning up the branch a bit and siloing ability popup work

--- src/battle/client.c
+++ src/battle/client.c
@@ -6,14 +6,9 @@
 
 #include "ui/battle.h"
 
-static void __attribute__((long_call)) Client_ShowAbility(struct Battle *battle, struct BattleClient *client);
-static void __attribute__((long_call)) Client_HideAbility(struct Battle *battle, struct BattleClient *client);
-
 extern const ClientFunc gClientFunctionTable_Old[]; // 0x0226F068
 
 const ClientFunc gClientFunctionTable_New[] = {
-    Client_ShowAbility,
-    Client_HideAbility,
 };
 
 void ClientCommand_Main(struct Battle *battle, struct BattleClient *client)
@@ -28,50 +23,4 @@ void ClientCommand_Main(struct Battle *battle, struct BattleClient *client)
             gClientFunctionTable_New[commandCode - CLIENT_NUM_VANILLA_MESSAGES](battle, client);
         }
     }
-}
-
-// ========================================================================= //
-//                           ABILITY POPUPS                                  //
-// ========================================================================= //
-
-struct WaitMessageTask {
-    void *battle;
-
-    u8   commandCode;
-    u8   clientNum;
-    u8   msgIndex;
-    u8   _dummyPadding;
-};
-
-static BOOL __attribute__((long_call)) CheckStillPrinting(u8 msgIndex); // 0x0201D6F8
-
-static void TaskControl_WaitMessage(void *taskControl, void *taskData)
-{
-    struct WaitMessageTask *wait = taskData;
-
-    if (CheckStillPrinting(wait->msgIndex) == FALSE) {
-        SCIO_ClearQueue(wait->battle, wait->clientNum, wait->commandCode);
-        Free(taskData);
-        TaskControl_Delete(taskControl);
-    }
-}
-
-static void Client_ShowAbility(struct Battle *battle, struct BattleClient *client)
-{
-    struct MessageParams *msgParams = (struct MessageParams*) &(client->commandBuffer[0]);
-    struct WaitMessageTask *wait    = Malloc(HEAP_ID_BATTLE, sizeof(struct WaitMessageTask));
-    wait->battle      = battle;
-    wait->commandCode = msgParams->commandCode;
-    wait->clientNum   = client->clientNum;
-    wait->msgIndex    = BattleUI_PrintPopupMessage(battle, msgParams);
-
-    TaskControl_Add(TaskControl_WaitMessage, wait, 0);
-    ClientCommand_Reset(client);
-}
-
-static void Client_HideAbility(struct Battle *battle, struct BattleClient *client)
-{
-    BattleUI_ClosePopupMessage();
-    
-    ClientCommand_Reset(client);
 }
--- src/battle/scrcmd.c
+++ src/battle/scrcmd.c
@@ -2,16 +2,6 @@
 #include "battle/scio.h"
 #include "battle/scrcmd.h"
 
-#if 0
-static BOOL __attribute__((long_call)) SkillSeq_LoadPopupGraphics(void *battle, struct BattleServer *server);
-static BOOL __attribute__((long_call)) SkillSeq_FreePopupGraphics(void *battle, struct BattleServer *server);
-static BOOL __attribute__((long_call)) SkillSeq_ShowPopup(void *battle, struct BattleServer *server);
-static BOOL __attribute__((long_call)) SkillSeq_HidePopup(void *battle, struct BattleServer *server);
-#endif
-
-static BOOL __attribute__((long_call)) BattleScrCmd_ShowAbility(void *battle, struct BattleServer *server);
-static BOOL __attribute__((long_call)) BattleScrCmd_HideAbility(void *battle, struct BattleServer *server);
-
 //#define DEBUG_BATTLE_SCRIPTS
 #define START_OF_NEW_BATTLE_SCRIPT_COMMANDS 0xDF
 
@@ -20,8 +10,6 @@ typedef BOOL (*BattleScrCmd)(void *battle, struct BattleServer *server);
 extern const BattleScrCmd gBattleScriptCommandTable_Old[];
 
 const BattleScrCmd gBattleScriptCommandTable_New[] = {
-    BattleScrCmd_ShowAbility,
-    BattleScrCmd_HideAbility,
 };
 
 BOOL BattleScrCmd_Exec(void *battle, struct BattleServer *server)
@@ -54,91 +42,3 @@ inline void IncScriptPtr(struct BattleServer *server, int incBy)
 {
     server->moveSeqNum += incBy;
 }
-
-// ========================================================================= //
-//                           ABILITY POPUPS                                  //
-// ========================================================================= //
-
-#define ABILITY_POPUP_MESSAGE_ID 1269   // TODO: move this to a better place
-
-static BOOL BattleScrCmd_ShowAbility(void *battle, struct BattleServer *server)
-{
-    IncScriptPtr(server, 1);
-
-    // Parameters:
-    //      - client number
-    int abilityClient = BattleScrCmd_ReadWord(server);
-    
-    struct MessageParams msgParams = {
-        .commandCode = CLIENT_SHOW_ABILITY,
-        .id          = ABILITY_POPUP_MESSAGE_ID,
-        .tag         = 11,  // TAG_NICK_ABILITY
-    };
-    msgParams.params[0] = abilityClient;
-    msgParams.params[1] = server->activePokemon[abilityClient].ability;
-
-    SCIO_Message(battle, SCIO_CLIENT, server->attacker, &msgParams, sizeof(struct MessageParams));
-
-    return FALSE;
-}
-
-static BOOL BattleScrCmd_HideAbility(void *battle, struct BattleServer *server)
-{
-    IncScriptPtr(server, 1);
-
-    int command = CLIENT_HIDE_ABILITY;
-    SCIO_Message(battle, SCIO_CLIENT, server->attacker, &command, 4);
-
-    return FALSE;
-}
-
-#if 0   // old ability popup stuff
-inline void IncScriptPtr(struct BattleServer *server, int incBy)
-{
-    server->moveSeqNum += incBy;
-}
-
-inline void SCIO_MessageClientCommandOnly(void *battle, struct BattleServer *server, int sendingClient, int command)
-{
-    IncScriptPtr(server, 1);
-
-    SCIO_Message(battle, SCIO_CLIENT, sendingClient, &command, 4);
-}
-
-BOOL SkillSeq_LoadPopupGraphics(void *battle, struct BattleServer *server)
-{
-    SCIO_MessageClientCommandOnly(battle, server, 0, CLIENT_POPUP_RESOURCE_LOAD);
-
-    return 0;
-}
-
-BOOL SkillSeq_FreePopupGraphics(void *battle, struct BattleServer *server)
-{
-    SCIO_MessageClientCommandOnly(battle, server, 0, CLIENT_POPUP_RESOURCE_FREE);
-
-    return 0;
-}
-
-BOOL SkillSeq_ShowPopup(void *battle, struct BattleServer *server)
-{
-    IncScriptPtr(server, 1);
-
-    int side   = ReadWord(server);
-    int client = BattlerClient(battle, server, side);
-    SCIO_MessageClientCommandOnly(battle, server, client, CLIENT_POPUP_IN);
-    
-    return 0;
-}
-
-BOOL SkillSeq_HidePopup(void *battle, struct BattleServer *server)
-{
-    IncScriptPtr(server, 1);
-
-    int side   = ReadWord(server);
-    int client = BattlerClient(battle, server, side);
-    SCIO_MessageClientCommandOnly(battle, server, client, CLIENT_POPUP_OUT);
-    
-    return 0;
-}
-
-#endif
