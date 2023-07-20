f361239c3ba6dcf15f0faeecb9c76d411314839b
Rachel <lhearachel@proton.me>
2023-07-20 15:11:03 -0700

    Hook in decomp'd code for CheckLeaveWith1HP

--- include/battle/scrcmd.h
+++ include/battle/scrcmd.h
@@ -16,6 +16,16 @@
 BOOL __attribute__((long_call)) BattleScrCmd_Exec(struct Battle *battle, struct BattleServer *server);
 
 /**
+ * @brief Gets the actual client number behind a battler definition.
+ * 
+ * @param battle
+ * @param server
+ * @param battlerDef    Refer to the BATTLER definitions from battle_scrcmd.s
+ * @return              The actual client number
+ */
+int  __attribute__((long_call)) BattleScrCmd_TrueBattler(struct Battle *battle, struct BattleServer *server, int battlerDef);
+
+/**
  * Reads a word from the scripting buffer.
  * 
  * Original function: 0x02248AD0 (overlay 16) 
--- src/battle/scrcmd.c
+++ src/battle/scrcmd.c
@@ -9,13 +9,14 @@
 #include "constants/item_hold_effects.h"
 #include "constants/misc.h"
 
+static BOOL __attribute__((long_call)) BattleScrCmd_CheckLeaveWith1HP(struct Battle *battle, struct BattleServer *server);
 static BOOL __attribute__((long_call)) BattleScrCmd_CheckPowderImmunity(struct Battle *battle, struct BattleServer *server);
 
 #define START_OF_NEW_BATTLE_SCRIPT_COMMANDS 0xDF
 
 typedef BOOL (*BattleScrCmd)(struct Battle *battle, struct BattleServer *server);
 
-extern const BattleScrCmd gBattleScriptCommandTable_Old[];
+extern const BattleScrCmd gBattleScriptCommandTable_Old[];  // Do NOT EVER change this to a pointer from an array or everything breaks
 
 const BattleScrCmd gBattleScriptCommandTable_New[] = {
     BattleScrCmd_CheckPowderImmunity,
@@ -23,32 +24,18 @@ const BattleScrCmd gBattleScriptCommandTable_New[] = {
 
 BOOL BattleScrCmd_Exec(struct Battle *battle, struct BattleServer *server)
 {
-    BOOL ret;
-    u32 word;
-#ifdef DEBUG_MODE
-    u8 buf[64];
-    sprintf(buf, "[PLAT-ENGINE] moveSeqNum: %ld\n", OFFSET_OF(struct BattleServer, moveSeqNum));
-    debugsyscall(buf);
-    sprintf(buf, "[PLAT-ENGINE] moveSeqWork: %ld\n", OFFSET_OF(struct BattleServer, moveSeqWork));
-    debugsyscall(buf);
-#endif
-    
+    BOOL result;
     do {
-        word = server->moveSeqWork[server->moveSeqNum];
-#ifdef DEBUG_MODE
-        sprintf(buf, "[PLAT-ENGINE] Got word: %ld\n", word);
-        debugsyscall(buf);
-#endif // DEBUG_BATTLE_SCRIPTS
-
+        u32 word = server->moveSeqWork[server->moveSeqNum];
         if (word < START_OF_NEW_BATTLE_SCRIPT_COMMANDS) {
-            ret = gBattleScriptCommandTable_Old[word](battle, server);
+            result = gBattleScriptCommandTable_Old[word](battle, server);
         } else {
-            ret = gBattleScriptCommandTable_New[word - START_OF_NEW_BATTLE_SCRIPT_COMMANDS](battle, server);
+            result = gBattleScriptCommandTable_New[word - START_OF_NEW_BATTLE_SCRIPT_COMMANDS](battle, server);
         }
     } while ((server->battleProgressFlag == 0) && ((Battle_Type(battle) & BATTLE_TYPE_LINK) == 0));
 
     server->battleProgressFlag = 0;
-    return ret;
+    return result;
 }
 
 inline void IncScriptPtr(struct BattleServer *server, int incBy)
@@ -56,15 +43,55 @@ inline void IncScriptPtr(struct BattleServer *server, int incBy)
     server->moveSeqNum += incBy;
 }
 
+static BOOL BattleScrCmd_CheckLeaveWith1HP(struct Battle *battle, struct BattleServer *server)
+{
+    IncScriptPtr(server, 1);
+
+    /*
+     * Args: battler
+     */
+    int battler = BattleScrCmd_ReadWord(server);
+    battler     = BattleScrCmd_TrueBattler(battle, server, battler);
+
+    int heldItemEffect = Server_HeldItemEffect(server, battler);
+    int heldItemPower  = Server_HeldItemPower(server, battler, 0);
+
+    BOOL triggered = FALSE;
+    if (heldItemEffect == HOLD_EFFECT_FOCUS_BAND && (Battle_Random(battle) % 100) < heldItemPower) {
+        triggered = TRUE;
+    }
+
+    if (heldItemEffect == HOLD_EFFECT_FOCUS_SASH
+            && server->activePokemon[battler].curHP == server->activePokemon[battler].maxHP) {
+        triggered = TRUE;
+    }
+
+    if (triggered) {
+        if (server->activePokemon[battler].curHP + server->hpCalcWork <= 0) {
+            server->hpCalcWork      = (server->activePokemon[battler].curHP - 1) * -1;
+            server->moveStatusFlag |= MOVE_STATUS_FLAG_HELD_ON_WITH_ITEM;
+        }
+    }
+
+    return FALSE;
+}
+
 static BOOL BattleScrCmd_CheckPowderImmunity(struct Battle *battle, struct BattleServer *server)
 {
     IncScriptPtr(server, 1);
 
+    /*
+     * Args: battler
+     *       jump distance if blocked by type
+     *       jump distance if blocked by ability
+     *       jump distance if blocked by item
+     */
     int battler              = BattleScrCmd_ReadWord(server);
     int jumpIfTypeBlocked    = BattleScrCmd_ReadWord(server);
     int jumpIfAbilityBlocked = BattleScrCmd_ReadWord(server);
     int jumpIfItemBlocked    = BattleScrCmd_ReadWord(server);
 
+    battler = BattleScrCmd_TrueBattler(battle, server, battler);
     if (Moves_IsPowder(server->moveWork)) {
         if ((server->activePokemon[battler].type1 == TYPE_GRASS)
                 || (server->activePokemon[battler].type2 == TYPE_GRASS)) {
