ccb87b05a63daab8b3d3be550ac4b2c15502eb1d
Lhea (Rachel) <lhearachel@proton.me>
2023-07-19 18:13:02 -0700

    Clean up the CheckAbilityOnHit code

--- src/battle/server.c
+++ src/battle/server.c
@@ -74,6 +74,18 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
         return FALSE;
     }
 
+    if (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) {
+        return FALSE;
+    }
+
+    if (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) {
+        return FALSE;
+    }
+
+    if (!DamageWasDealt(server)) {
+        return FALSE;
+    }
+
     BOOL result = FALSE;
     switch (Server_Ability(server, server->defender)) {
     case ABILITY_STATIC:
@@ -81,14 +93,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
         if (server->activePokemon[server->attacker].curHP
                 // Attacker has no present status condition
                 && server->activePokemon[server->attacker].condition == 0
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move (does this matter?)
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
-                // Damage was actually dealt
-                && DamageWasDealt(server)
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT
                 // Activate 30% of the time
@@ -103,15 +107,9 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
     case ABILITY_COLOR_CHANGE:
         // Defender is still alive after the attack
         if (server->activePokemon[server->defender].curHP
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
                 // The move is not Struggle
                 && server->moveIDCurr != MOVE_STRUGGLE
-                // Damage was actually dealt
-                && DamageWasDealt(server)
-                // Move actually has a base power (seems superfluous with the above?)
+                // Move actually has a base power
                 && server->aiWork.moveTable[server->moveIDCurr].power
                 // The move's type does not match either of the user's current types
                 && BattlePokemon_Get(server, server->defender, BATTLE_MON_PARAM_TYPE_1, NULL) != server->moveType
@@ -120,18 +118,11 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
             *seqNum = SUBSCR_COLOR_CHANGE;
             result  = TRUE;
         }
-        break;
     case ABILITY_ROUGH_SKIN:
         // Attacker is still alive after the attack
         if (server->activePokemon[server->attacker].curHP
                 // Attacker does not have Magic Guard
                 && Server_Ability(server, server->attacker) != ABILITY_MAGIC_GUARD
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
                 // Damage was actually dealt
                 && DamageWasDealt(server)
                 // Move makes contact
@@ -147,14 +138,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
         if (server->activePokemon[server->attacker].curHP
                 // Attacker has no present status condition
                 && server->activePokemon[server->attacker].condition == 0
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
-                // Damage was actually dealt
-                && DamageWasDealt(server)
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT
                 // Activate 30% of the time
@@ -177,14 +160,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
         if (server->activePokemon[server->attacker].curHP
                 // Attacker has no present status condition
                 && server->activePokemon[server->attacker].condition == 0
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
-                // Damage was actually dealt
-                && DamageWasDealt(server)
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT
                 // Activate 30% of the time
@@ -201,14 +176,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
         if (server->activePokemon[server->attacker].curHP
                 // Attacker has no present status condition
                 && server->activePokemon[server->attacker].condition == 0
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
-                // Damage was actually dealt
-                && DamageWasDealt(server)
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT
                 // Activate 30% of the time
@@ -227,14 +194,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
                 && server->activePokemon[server->defender].curHP
                 // Attacker is not currently infatuated
                 && (server->activePokemon[server->attacker].condition2 & CONDITION_V_INFATUATED) == 0
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // It is not turn one of a two-turn move
-                && (server->serverStatusFlag & SERVER_STATUS_FLAG_TURN_ONE_OF_TWO) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
-                // Damage was actually dealt
-                && DamageWasDealt(server)
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT
                 // Activate 30% of the time
@@ -255,10 +214,6 @@ BOOL Server_CheckAbilityOnHit(struct Battle *battle, struct BattleServer *server
                 && Server_Ability(server, server->attacker) != ABILITY_MAGIC_GUARD
                 // Damp is not active
                 && Server_CheckAbility(battle, server, CHECK_ABILITY_ACTIVE, 0, ABILITY_DAMP) == FALSE
-                // The move should activate any effects after hitting
-                && (server->moveStatusFlag & MOVE_STATUS_FLAG_NO_HIT_EFFECTS) == 0
-                // The move is not a pivoting move (there is a separate checker for that)
-                && (server->serverStatusFlag2 & SERVER_STATUS_FLAG2_PIVOT_MOVE) == 0
                 // Move makes contact
                 && server->aiWork.moveTable[server->moveIDCurr].flag & MOVE_FLAG_MAKES_CONTACT) {
             server->hpCalcWork = Server_DivideBy(server->activePokemon[server->attacker].maxHP * -1, 4);
