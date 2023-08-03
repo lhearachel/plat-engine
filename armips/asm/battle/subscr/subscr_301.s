4c03473321dea8925567952a9b8455b6118590c9
Rachel <lhearachel@proton.me>
2023-08-03 13:50:25 -0700

    Cleanup subscr_301.s for clarity

--- armips/asm/battle/subscr/subscr_301.s
+++ armips/asm/battle/subscr/subscr_301.s
@@ -15,16 +15,16 @@
 .create "build/battle/scr/subscr/sub_seq_301.bin", 0
 
 // SUBSCR_SAP_SIPPER
-subscr_183:
+subscr_301:
     AttackMessage                       
     Wait                                
     WaitTime                            15
-    IfMonData                           EQUAL, BATTLER_DEFENDER, BATTLE_MON_ATK_STAGE, 12, subscr_183_AlreadyMaxed
+    IfMonData                           EQUAL, BATTLER_DEFENDER, BATTLE_MON_ATK_STAGE, 12, subscr_301_AlreadyMaxed
     SetStatusEffect                     BATTLER_DEFENDER, STATUS_STAT_BOOST
     Wait                                
-    // Raise special attack by 1 stage
+    // Raise attack by 1 stage
     SetMonData                          OP_ADD, BATTLER_DEFENDER, BATTLE_MON_ATK_STAGE, 1
-    // Specify special attack for the message
+    // Specify attack for the message
     SetVar                              OP_SET, VAR_PREPARED_MESSAGE, STAT_ATTACK
     // "{0}'s {1} raised its {2}!"
     Message                             622, TAG_NICK_ABILITY_STAT, BATTLER_DEFENDER, BATTLER_DEFENDER, BATTLER_WORKING, NaN, NaN, NaN
@@ -32,7 +32,7 @@ subscr_183:
     WaitTime                            30
     End                                 
 
-subscr_183_AlreadyMaxed:
+subscr_301_AlreadyMaxed:
     // "{0}'s {1} made {2} useless!"
     Message                             638, TAG_NICK_ABILITY_MOVE, BATTLER_DEFENDER, BATTLER_DEFENDER, BATTLER_ATTACKER, NaN, NaN, NaN
     Wait                                
